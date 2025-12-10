8571b0fff7c0d30312ebbcc21f504cd34862e74d
hassan <hassanhamadrasool@gmail.com>
2025-12-10 08:04:05 +0500

    HASSAN HAMAD RASOOL

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      hassan page
+      hassan about page
     </div>
   )
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      hassan page
+      hassan contact page
+      
     </div>
   )
 }
